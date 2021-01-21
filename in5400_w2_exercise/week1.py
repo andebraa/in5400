9aea84540da42d3a36afe2634c2519d65b0bf959
anders <andebraa@uio.no>
2021-01-21 10:26:44 +0100

    week1 update

---
+++ in5400_w2_exercise/week1.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def loops(X, T):
+    d = np.empty((len(X[0,:]), len(T[0,:])))
+    print(d.shape)
+    for i in range(len(X[0,:])):
+        for j in range(len(T[0,:])):
+            d[i,j]= np.linalg.norm(X[i,:] - T[j,:])
+
+    return d
+
+def numpy_solution(X,T):
+    d = np.empty((len(X[0,:]), len(T[0,:])))
+    d = (X**2).sum(axis = 1, keepdims=True) +\
+        (T**2).sum(axis=1)+\
+        2*X @ T.T
+
+    return d
+
+D = 5
+N = 10
+P = 5
+
+X = np.random.randint(10,size=(N,D))
+T = np.random.randint(10,size=(P,D))
+
+d = numpy_solution(X,T)
+print(d)
+
+d = loops(X,T)
+print(d)
