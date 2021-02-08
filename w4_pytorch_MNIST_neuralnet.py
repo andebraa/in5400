982eb59c937fb3ec56686c497ab5d7f3f8b9c0f8
anders <andebraa@uio.no>
2021-02-08 12:49:34 +0100

    example code from week 4. correctly implemented

--- w4_pytorch_MNIST_neuralnet.py
+++ w4_pytorch_MNIST_neuralnet.py
@@ -67,3 +67,173 @@ class Model(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+# Create an instance of Model
+model = Model()
+if config['use_cuda'] == True:
+    model.to('cuda')
+
+optimizer=torch.optim.SGD(model.parameters(),lr=config['learningRate'], momentum=0.0, weight_decay=0)
+
+def loss_fn(prediction, labels):
+    """Returns softmax cross entropy loss."""
+    loss = F.cross_entropy(input=prediction, target=labels)
+    return loss
+
+def run_epoch(model, epoch, data_loader, optimizer, is_training, config):
+    """
+    Args:
+        model        (obj): The neural network model
+        epoch        (int): The current epoch
+        data_loader  (obj): A pytorch data loader "torch.utils.data.DataLoader"
+        optimizer    (obj): A pytorch optimizer "torch.optim"
+        is_training (bool): Whether to use train (update) the model/weights or not.
+        config      (dict): Configuration parameters
+
+    Intermediate:
+        totalLoss: (float): The accumulated loss from all batches.
+                            Hint: Should be a numpy scalar and not a pytorch scalar
+
+    Returns:
+        loss_avg         (float): The average loss of the dataset
+        accuracy         (float): The average accuracy of the dataset
+        confusion_matrix (float): A 10x10 matrix
+    """
+
+    if is_training==True:
+        model.train()
+    else:
+        model.eval()
+
+    total_loss       = 0
+    correct          = 0
+    confusion_matrix = np.zeros(shape=(10,10))
+    labels_list      = [0,1,2,3,4,5,6,7,8,9]
+
+    for batch_idx, data_batch in enumerate(data_loader):
+        if config['use_cuda'] == True:
+            images = data_batch[0].to('cuda') # send data to GPU
+            labels = data_batch[1].to('cuda') # send data to GPU
+        else:
+            images = data_batch[0]
+            labels = data_batch[1]
+
+        if not is_training:
+            with torch.no_grad():
+                prediction = model(images)
+                loss = loss_fn(prediction, labels)
+                total_loss += loss.item()
+        elif is_training:
+            prediction = model(images)
+            loss = loss_fn(prediction, labels)
+            total_loss += loss.item()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            #raise NotImplementedError('Do a gradient descent step by 1) setting the gradients to zero, 2) performing backpropagation, and 3) updating the parameters.')
+
+
+
+        # Update the number of correct classifications and the confusion matrix
+        predicted_label  = prediction.max(1, keepdim=True)[1][:,0]
+        correct          += predicted_label.eq(labels).cpu().sum().numpy()
+        confusion_matrix += metrics.confusion_matrix(labels.cpu().numpy(), predicted_label.cpu().numpy(), labels=labels_list)
+
+        # Print statistics
+        #batchSize = len(labels)
+        if batch_idx % config['log_interval'] == 0:
+            print(f'Epoch={epoch} | {(batch_idx+1)/len(data_loader)*100:.2f}% | loss = {loss:.5f}')
+
+    loss_avg         = total_loss / len(data_loader)
+    accuracy         = correct / len(data_loader.dataset)
+    confusion_matrix = confusion_matrix / len(data_loader.dataset)
+
+    return loss_avg, accuracy, confusion_matrix
+
+
+# train the model
+train_loss = np.zeros(shape=config['epochs'])
+train_acc  = np.zeros(shape=config['epochs'])
+val_loss   = np.zeros(shape=config['epochs'])
+val_acc    = np.zeros(shape=config['epochs'])
+train_confusion_matrix = np.zeros(shape=(10,10,config['epochs']))
+val_confusion_matrix   = np.zeros(shape=(10,10,config['epochs']))
+
+for epoch in range(config['epochs']):
+    train_loss[epoch], train_acc[epoch], train_confusion_matrix[:,:,epoch] = \
+                               run_epoch(model, epoch, train_loader, optimizer, is_training=True, config=config)
+
+    val_loss[epoch], val_acc[epoch], val_confusion_matrix[:,:,epoch]     = \
+                               run_epoch(model, epoch, val_loader, optimizer, is_training=False, config=config)
+
+# Plot the loss and the accuracy in training and validation
+#plt.figure()
+plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+ax = plt.subplot(2, 1, 1)
+# plt.subplots_adjust(hspace=2)
+ax.plot(train_loss, 'b', label='train loss')
+ax.plot(val_loss, 'r', label='validation loss')
+ax.grid()
+plt.ylabel('Loss', fontsize=18)
+plt.xlabel('Epochs', fontsize=18)
+ax.legend(loc='upper right', fontsize=16)
+
+ax = plt.subplot(2, 1, 2)
+plt.subplots_adjust(hspace=0.4)
+ax.plot(train_acc, 'b', label='train accuracy')
+ax.plot(val_acc, 'r', label='validation accuracy')
+ax.grid()
+plt.ylabel('Accuracy', fontsize=18)
+plt.xlabel('Epochs', fontsize=18)
+val_acc_max = np.max(val_acc)
+val_acc_max_ind = np.argmax(val_acc)
+plt.axvline(x=val_acc_max_ind, color='g', linestyle='--', label='Highest validation accuracy')
+plt.title('Highest validation accuracy = %0.1f %%' % (val_acc_max*100), fontsize=16)
+ax.legend(loc='lower right', fontsize=16)
+plt.ion()
+
+
+ind = np.argmax(val_acc)
+class_accuracy = val_confusion_matrix[:,:,ind]
+for ii in range(len(classes)):
+    acc = val_confusion_matrix[ii,ii,ind] / np.sum(val_confusion_matrix[ii,:,ind])
+    print(f'Accuracy of {str(classes[ii]).ljust(15)}: {acc*100:.01f}%')
+
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+epoch_step                  = 2
+set_colorbar_max_percentage = 10
+
+# Plot confusion matrices
+ticks = np.linspace(0,9,10)
+gridspec_kwargs = dict(top=0.9, bottom=0.1, left=0.0, right=0.9, wspace=0.5, hspace=0.2)
+for i in range(0, config['epochs'], epoch_step):
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 16), gridspec_kw=gridspec_kwargs)
+    im = ax1.imshow(val_confusion_matrix[:, :, i]*100)
+    ax1.set_title(f'Validation: Epoch #{i}', fontsize=18)
+    ax1.set_xticks(ticks=ticks)
+    ax1.set_yticks(ticks=ticks)
+    ax1.set_yticklabels(classes)
+    im.set_clim(0.0, set_colorbar_max_percentage)
+    ax1.set_xticklabels(classes, rotation=45)
+    ax1.set_ylabel('Prediction', fontsize=16)
+    ax1.set_xlabel('Ground truth', fontsize=16)
+    divider = make_axes_locatable(ax1)
+    cax     = divider.append_axes('right', size='5%', pad=0.15)
+    f.colorbar(im, cax=cax, orientation='vertical')
+
+    im = ax2.imshow(train_confusion_matrix[:, :, i]*100)
+    ax2.set_title(f'Train: Epoch #{i}', fontsize=18)
+    ax2.set_xticks(ticks=ticks)
+    ax2.set_yticks(ticks=ticks)
+    ax2.set_yticklabels(classes)
+    im.set_clim(0.0, set_colorbar_max_percentage)
+    ax2.set_xticklabels(classes, rotation=45)
+    ax2.set_ylabel('Prediction', fontsize=16)
+    ax2.set_xlabel('Ground truth', fontsize=16)
+    divider = make_axes_locatable(ax2)
+    cax     = divider.append_axes('right', size='5%', pad=0.15)
+    f.colorbar(im, cax=cax, orientation='vertical')
