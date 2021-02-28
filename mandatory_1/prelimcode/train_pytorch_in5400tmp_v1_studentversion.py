194db336f886a0fc373bc46b62a004d33e3befd4
andebraa <andebraa@uio.no>
2021-02-28 17:23:53 +0100

    attempt at loading files. not done yet

--- mandatory_1/prelimcode/train_pytorch_in5400tmp_v1_studentversion.py
+++ mandatory_1/prelimcode/train_pytorch_in5400tmp_v1_studentversion.py
@@ -26,20 +26,62 @@ from typing import Callable, Optional
 
 
 class dataset_voc(Dataset):
-  def __init__(self, root_dir, trvaltest, transform=None):
+    def __init__(self, root_dir, trvaltest, transform=None):
 
-    #TODO 
-    #read in pascal VOC dataset
+        self.root_dir = root_dir
 
-  def __len__(self):
-      return len(self.imgfilenames)
+        self.transform = transform
+        self.imgfilenames=[]
+        self.labels=[]
 
-  def __getitem__(self, idx):
-    #TODO your code here
 
-    sample = {'image': image, 'label': label, 'filename': self.imgfilenames[idx]}
+        if trvaltest==0:
+            #load training data
+            file = open("./102flowersn/flowers_data/trainfile.txt")
+            path = "./102flowersn/flowers_data/jpg/"
+            for i, line in enumerate(file):
+                image = line.split()[0]
+                flower = int(line.split()[1])
+                self.imgfilenames.append(path + image)
+                self.labels.append(flower)
+            file.close()
+        elif trvaltest==1:
+            #load validation data
+            file = open("./102flowersn/flowers_data/valfile.txt")
+            path = "./102flowersn/flowers_data/jpg/"
+            for i, line in enumerate(file):
+                image = line.split()[0]
+                flower = int(line.split()[1])
+                self.imgfilenames.append(path + image)
+                self.labels.append(flower)
+            file.close()
+        elif trvaltest==2:
+            #load test data
+            file = open("./102flowersn/flowers_data/testfile.txt")
+            path = "./102flowersn/flowers_data/jpg/"
+            for i, line in enumerate(file):
+                image = line.split()[0]
+                flower = int(line.split()[1])
+                self.imgfilenames.append(path + image)
+                self.labels.append(flower)
+            file.close()
+        #TODO
+        else:
+            #TODO: print some error + exit() or an exception
+            raise ValueError('init error')
 
-    return sample
+
+
+
+    def __len__(self):
+        return len(self.imgfilenames)
+
+    def __getitem__(self, idx):
+        #TODO your code here
+
+        sample = {'image': image, 'label': label, 'filename': self.imgfilenames[idx]}
+
+        return sample
 
 
 
@@ -47,101 +89,100 @@ def train_epoch(model,  trainloader,  criterion, device, optimizer ):
 
     #TODO
     #model.train() or model.eval() ?
- 
+
     losses = []
     for batch_idx, data in enumerate(trainloader):
-      #TODO
-      
+        #TODO
+
     return np.mean(losses)
-    
-    
+
+
 
 def evaluate_meanavgprecision(model, dataloader, criterion, device, numcl):
 
     #TODO
     #model.train() or model.eval() ?
- 
+
 
 
     #curcount = 0
-    #accuracy = 0 
-    
+    #accuracy = 0
+
     concat_pred=[np.empty(shape=(0)) for _ in range(numcl)] #prediction scores for each class. each numpy array is a list of scores. one score per image
     concat_labels=[np.empty(shape=(0)) for _ in range(numcl)] #labels scores for each class. each numpy array is a list of labels. one label per image
     avgprecs=np.zeros(numcl) #average precision for each class
     fnames = [] #filenames as they come out of the dataloader
-    
-    
+
+
     with torch.no_grad():
-      losses = []
-      for batch_idx, data in enumerate(dataloader):
-      
-      
-          if (batch_idx%100==0) and (batch_idx>=100):
-            print('at val batchindex: ',batch_idx)
-      
-          inputs = data['image'].to(device)        
-          outputs = model(inputs)
+        losses = []
+        for batch_idx, data in enumerate(dataloader):
 
-          labels = data['label']
+            if (batch_idx%100==0) and (batch_idx>=100):
+                print('at val batchindex: ',batch_idx)
 
-          loss = criterion(outputs, labels.to(device) )
-          losses.append(loss.item())
-          
-          #this was an accuracy computation
-          #cpuout= outputs.to('cpu')
-          #_, preds = torch.max(cpuout, 1)
-          #labels = labels.float()
-          #corrects = torch.sum(preds == labels.data)
-          #accuracy = accuracy*( curcount/ float(curcount+labels.shape[0]) ) + corrects.float()* ( curcount/ float(curcount+labels.shape[0]) )
-          #curcount+= labels.shape[0]
-          
-          #TODO: collect scores, labels, filenames
-          
+            inputs = data['image'].to(device)
+            outputs = model(inputs)
 
-    
-    for c in range(numcl):   
-      avgprecs[c]= #TODO
-      
+            labels = data['label']
+
+            loss = criterion(outputs, labels.to(device) )
+            losses.append(loss.item())
+
+            #this was an accuracy computation
+            #cpuout= outputs.to('cpu')
+            #_, preds = torch.max(cpuout, 1)
+            #labels = labels.float()
+            #corrects = torch.sum(preds == labels.data)
+            #accuracy = accuracy*( curcount/ float(curcount+labels.shape[0]) ) + corrects.float()* ( curcount/ float(curcount+labels.shape[0]) )
+            #curcount+= labels.shape[0]
+
+            #TODO: collect scores, labels, filenames
+
+
+
+    for c in range(numcl):
+        avgprecs[c]= #TODO
+
     return avgprecs, np.mean(losses), concat_labels, concat_pred, fnames
 
 
 def traineval2_model_nocv(dataloader_train, dataloader_test ,  model ,  criterion, optimizer, scheduler, num_epochs, device, numcl):
 
-  best_measure = 0
-  best_epoch =-1
+    best_measure = 0
+    best_epoch =-1
 
-  trainlosses=[]
-  testlosses=[]
-  testperfs=[]
-  
-  for epoch in range(num_epochs):
-    print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-    print('-' * 10)
+    trainlosses=[]
+    testlosses=[]
+    testperfs=[]
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
 
 
-    avgloss=train_epoch(model,  dataloader_train,  criterion,  device , optimizer )
-    trainlosses.append(avgloss)
-    
+        avgloss=train_epoch(model,  dataloader_train,  criterion,  device , optimizer )
+        trainlosses.append(avgloss)
+
     if scheduler is not None:
-      scheduler.step()
+        scheduler.step()
 
     perfmeasure, testloss,concat_labels, concat_pred, fnames  = evaluate_meanavgprecision(model, dataloader_test, criterion, device, numcl)
     testlosses.append(testloss)
     testperfs.append(perfmeasure)
-    
+
     print('at epoch: ', epoch,' classwise perfmeasure ', perfmeasure)
-    
+
     avgperfmeasure = np.mean(perfmeasure)
     print('at epoch: ', epoch,' avgperfmeasure ', avgperfmeasure)
 
     if avgperfmeasure > best_measure: #higher is better or lower is better?
-      bestweights= model.state_dict()
-      #TODO track current best performance measure and epoch
-      
-      #TODO save your scores
+        bestweights= model.state_dict()
+        #TODO track current best performance measure and epoch
 
-  return best_epoch, best_measure, bestweights, trainlosses, testlosses, testperfs
+        #TODO save your scores
+
+    return best_epoch, best_measure, bestweights, trainlosses, testlosses, testperfs
 
 
 
@@ -150,9 +191,9 @@ class yourloss(nn.modules.loss._Loss):
 
     def __init__(self, reduction: str = 'mean') -> None:
         #TODO
-        
+
     def forward(self, input_: Tensor, target: Tensor) -> Tensor:
-        
+
         #TODO
         return loss
 
@@ -163,26 +204,26 @@ class yourloss(nn.modules.loss._Loss):
 def runstuff():
 
 
-  config = dict()
-  
-  config['use_gpu'] = False #True #TODO change this to True for training on the cluster, eh
-  config['lr']=0.005
-  config['batchsize_train'] = 16
-  config['batchsize_val'] = 64
-  config['maxnumepochs'] = 35
+    config = dict()
 
-  config['scheduler_stepsize']=10
-  config['scheduler_factor']=0.3
+    config['use_gpu'] = False #True #TODO change this to True for training on the cluster, eh
+    config['lr']=0.005
+    config['batchsize_train'] = 16
+    config['batchsize_val'] = 64
+    config['maxnumepochs'] = 35
 
-
-  
-  # kind of a dataset property
-  config['numcl']=20
+    config['scheduler_stepsize']=10
+    config['scheduler_factor']=0.3
 
 
 
-  #data augmentations
-  data_transforms = {
+    # kind of a dataset property
+    config['numcl']=20
+
+
+
+    #data augmentations
+    data_transforms = {
       'train': transforms.Compose([
           transforms.Resize(256),
           transforms.RandomCrop(224),
@@ -197,47 +238,47 @@ def runstuff():
           transforms.ToTensor(),
           transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
       ]),
-  }
+    }
 
 
-  #datasets
-  image_datasets={}
-  image_datasets['train']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=0, transform=data_transforms['train'])
-  image_datasets['val']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=1, transform=data_transforms['val'])
+    #datasets
+    image_datasets={}
+    image_datasets['train']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=0, transform=data_transforms['train'])
+    image_datasets['val']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=1, transform=data_transforms['val'])
 
-  #dataloaders
-  #TODO use num_workers=1
-  dataloaders = {}
-  dataloaders['train'] = #
-  dataloaders['val'] = #
-  
-
-  #device
-  if True == config['use_gpu']:
-      device= torch.device('cuda:0')
-
-  else:
-      device= torch.device('cpu')
-
-  #model
-  #TODO
-  model = #pretrained resnet18
-  #overwrite last linear layer
-  
-  model = model.to(device)
+    #dataloaders
+    #TODO use num_workers=1
+    dataloaders = {}
+    dataloaders['train'] = #
+    dataloaders['val'] = #
 
 
-  lossfct = yourloss()
-  
-  #TODO
-  # Observe that all parameters are being optimized
-  someoptimizer = #
+    #device
+    if True == config['use_gpu']:
+        device= torch.device('cuda:0')
 
-  # Decay LR by a factor of 0.3 every X epochs
-  #TODO
-  somelr_scheduler = #
+    else:
+        device= torch.device('cpu')
 
-  best_epoch, best_measure, bestweights, trainlosses, testlosses, testperfs = traineval2_model_nocv(dataloaders['train'], dataloaders['val'] ,  model ,  lossfct, someoptimizer, somelr_scheduler, num_epochs= config['maxnumepochs'], device = device , numcl = config['numcl'] )
+    #model
+    #TODO
+    model = #pretrained resnet18
+    #overwrite last linear layer
+
+    model = model.to(device)
+
+
+    lossfct = yourloss()
+
+    #TODO
+    # Observe that all parameters are being optimized
+    someoptimizer = #
+
+    # Decay LR by a factor of 0.3 every X epochs
+    #TODO
+    somelr_scheduler = #
+
+    best_epoch, best_measure, bestweights, trainlosses, testlosses, testperfs = traineval2_model_nocv(dataloaders['train'], dataloaders['val'] ,  model ,  lossfct, someoptimizer, somelr_scheduler, num_epochs= config['maxnumepochs'], device = device , numcl = config['numcl'] )
 
 
 
@@ -264,7 +305,7 @@ def setbyname2(targetmodel,name,value):
         return True
       else:
         nextobj=getattr(obj,components[0])
-        
+
         newtail = nametail
         newtail.append(components[0])
         #print('components ',components, nametail, newtail)
@@ -275,9 +316,9 @@ def setbyname2(targetmodel,name,value):
     components=name.split('.')
     success=iteratset(targetmodel,components,value, nametail=[])
     return success
-    
-    
- 
+
+
+
 class wsconv2(nn.Conv2d):
   def __init__(self, in_channels, out_channels, kernel_size, stride,
                      padding, dilation = 1 , groups =1 , bias = None, eps=1e-12 ):
@@ -287,43 +328,43 @@ class wsconv2(nn.Conv2d):
 
   def forward(self,x):
     #torch.nn.functional.conv2d documentation tells about weight shapes
-    pass     
-      
+    pass
+
 
 def bntoWSconverter(model):
 
   #either you modify model in place
-  #or you create a copy of it e.g. using copy.deepcopy(...)  
+  #or you create a copy of it e.g. using copy.deepcopy(...)
   # https://discuss.pytorch.org/t/are-there-any-recommended-methods-to-clone-a-model/483/17
 
   lastwasconv2= False
   for nm,module in model.named_modules():
     #print(nm)
-    
+
     if isinstance(module, nn.Conv2d):
       #replace, get std
       lastwasconv2= True
-      
+
       usedeps= 1e-12 # use 1e-12 if you add it to a variance term, and 1e-6 if you add it to a standard deviation term
-      
-      #TODO 
+
+      #TODO
       # put in here your wsconv2, dont forget to copy convolution weight and, if exists, the convolution bias into your wsconv2
-      
-      setbyname2(model,nm,newconv)            
-      
+
+      setbyname2(model,nm,newconv)
+
     elif isinstance(module,nn.BatchNorm2d):
-    
+
       if False == lastwasconv2:
         print('got disconnected batchnorm??')
         exit()
-        
-    
+
+
       print('got one', nm)
-      
-      #TODO 
+
+      #TODO
       # you will need here data computed from the preceding nn.Conv2d instance which came along your way
 
-      #delete       
+      #delete
       lastwasconv2= False
 
     else:
@@ -372,17 +413,17 @@ class dataset_imagenetvalpart(Dataset):
           label,firstname=parseclasslabel(self.filenametoxml(nm) ,self.synsetstoindices)
           self.labels.append(label)
 
-   
+
   def filenametoxml(self,fn):
     f=os.path.basename(fn)
-     
+
     if not f.endswith(self.ending):
       print('not f.endswith(self.ending)')
       exit()
-       
+
     f=f[:-len(self.ending)]+'.xml'
-    f=os.path.join(self.xmllabeldir,f) 
-     
+    f=os.path.join(self.xmllabeldir,f)
+
     return f
 
 
@@ -405,7 +446,7 @@ class dataset_imagenetvalpart(Dataset):
 
 
 
- 
+
 def comparetwomodeloutputs(model1, model2, dataloader, device):
 
     model1.eval()
@@ -413,41 +454,41 @@ def comparetwomodeloutputs(model1, model2, dataloader, device):
 
     curcount = 0
     avgdiff = 0
-    
+
     with torch.no_grad():
       for batch_idx, data in enumerate(dataloader):
-      
-      
+
+
           if (batch_idx%100==0) and (batch_idx>=100):
               print('at val batchindex: ', batch_idx)
-      
-          inputs = data['image'].to(device)        
+
+          inputs = data['image'].to(device)
           outputs1 = model1(inputs)
           outputs2 = model2(inputs)
 
-          diff=torch.mean(torch.abs((outputs1-outputs2).flatten())) 
-          
+          diff=torch.mean(torch.abs((outputs1-outputs2).flatten()))
+
           labels = data['label']
           print('diff',diff.item())
           avgdiff = avgdiff*( curcount/ float(curcount+labels.shape[0]) ) + diff.item()* ( labels.shape[0]/ float(curcount+labels.shape[0]) )
-          
+
 
           curcount+= labels.shape[0]
 
     return avgdiff
 
-  
+
 #routine to test that your copied model at evaluation time works as intended
 def test_WSconversion():
-  
+
 
   config = dict()
-  
+
   #config['use_gpu'] = True
   #config['lr']=0.008 #0.005
   #config['batchsize_train'] = 2
   #config['batchsize_val'] = 64
-  
+
   #data augmentations
   data_transforms = {
       'val': transforms.Compose([
@@ -458,44 +499,42 @@ def test_WSconversion():
           transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
       ]),
   }
-  
+
   root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/imagenet300/'
   xmllabeldir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/val/'
   synsetfile='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/students/synset_words.txt'
 
   dset= dataset_imagenetvalpart(root_dir, xmllabeldir, synsetfile, maxnum=64, transform=data_transforms['val'])
-  dataloader =  torch.utils.data.DataLoader(dset, batch_size=64, shuffle=False) #, num_workers=1) 
+  dataloader =  torch.utils.data.DataLoader(dset, batch_size=64, shuffle=False) #, num_workers=1)
 
   import copy
-  device=torch.device('cpu')  
+  device=torch.device('cpu')
   #model
   model = models.resnet18(pretrained=True)
   model2 = copy.deepcopy(model.to('cpu'))
-  
+
   ####################
   # assumes it changes the model in-place, use model2= bntoWSconverter(model) if your routine instead modifies a copy of model and returns it
   ######################
   bntoWSconverter(model2)
-  
-  model = model.to(device)  
-  model2 = model2.to(device)  
-  
-  avgdiff = comparetwomodeloutputs(model, model2, dataloader, device)
-  
 
-  
+  model = model.to(device)
+  model2 = model2.to(device)
+
+  avgdiff = comparetwomodeloutputs(model, model2, dataloader, device)
+
+
+
   print('model checking averaged difference', avgdiff )  # order 1e-3 is okay, 1e-2 is still okay.
-  
-    
-    
-    
+
+
+
+
 '''
-    
+
 
 
 
 if __name__=='__main__':
 
   runstuff()
-
-
